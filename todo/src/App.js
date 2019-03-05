5773b1e9cc2d1b90eff8f382562cae7e2ec59cea
Frank Martinez <frankie.martinez@gmail.com>
2019-03-04 17:57:55 -0800

    progress on basic scaffolding of application

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { addTodo, completeTodo, clearTodo } from './components/actions';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className='brand-title'>Todo list</h1>
+
+
       </div>
     );
   }
 }
 
-export default App;
+// This function states what part of the state tree should be exposed to this 
+// component.
+const mapStateToProps = (state) => {
+  return {
+      todoList: state
+  };
+};
+
+// HOC that makes this component aware of the rest of the redux architecture.
+// Without this, this component is only a dumb react component.
+// We pass in all the functions that are reliant on Redux along with the 
+// component itself, so that redux makes itself known to this component.
+export default connect(mapStateToProps, {addTodo, completeTodo, clearTodo})(App);
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import banana from './components/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './css/index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const store = createStore(banana);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+
+
+
